1bc2846b2d96498e09392f8f6f6ec48c72912367
Daniel O'Keefe <dantokeefe@gmail.com>
2021-04-28 19:09:07 -0400

    accept JSON payload

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import numpy as np
 import pandas as pd
 from fastapi import FastAPI
 from tensorflow import keras
+from pydantic import BaseModel
 
+class Iris(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+        
 # Load model
 model = keras.models.load_model('iris_model')
 
 # Load input scaling parameters
 with open('scaling.json') as f:
-    d = json.load(f)
+    s = json.load(f)
 
 app = FastAPI()
 
-
 @app.get("/predict")
-async def get_predition(f0: float, f1: float, f2: float, f3: float):
-    f0 = (f0 - d['means'][0]) / np.sqrt(d['vars'][0])
-    f1 = (f1 - d['means'][1]) / np.sqrt(d['vars'][1])
-    f2 = (f2 - d['means'][2]) / np.sqrt(d['vars'][2])
-    f3 = (f3 - d['means'][3]) / np.sqrt(d['vars'][3])
+async def get_predition(iris: Iris):
+    f0 = (iris.sepal_length - s['means'][0]) / np.sqrt(s['vars'][0])
+    f1 = (iris.sepal_width - s['means'][1]) / np.sqrt(s['vars'][1])
+    f2 = (iris.petal_length - s['means'][2]) / np.sqrt(s['vars'][2])
+    f3 = (iris.petal_width - s['means'][3]) / np.sqrt(s['vars'][3])
     X_scaled = [[f0, f1, f2, f3]]
     
     y_pred = model.predict(X_scaled)
